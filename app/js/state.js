f65a04c558469ee9b56b8967d9a288acbb445433
luisepultura <luisepultura@hotmail.com>
2017-03-21 21:49:29 +0000

    created lessons reducer.

--- app/js/state.js
+++ app/js/state.js
@@ -1,3 +1,5 @@
+import { lessons } from './library'
+
 //store
 const createStore = reducer => {
     let state;
@@ -26,33 +28,70 @@ const createStore = reducer => {
     };
 };
 
+const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        return Object.keys(reducers).reduce(
+            (nextState, key) => {
+                nextState[key] = reducers[key](state[key], action);
+                return nextState;
+            },
+            {}
+        );
+    };
+};
 
 //reducers
 
 const visibilityFilter = (state = 'HIDE_ALL', action) => {
+    const { code } = action;
     switch (action.type) {
         case 'NEXT':
-            const { index, lessonId } = action;
+
             return [
 
             ];
         case 'PREVIOUS':
-            const { index, lessonId } = action;
             return action.filter;
         case 'GO_TO':
-            const { lessonId } = action;
             return action.filter;
         default:
             return state;
     }
 };
 
-const app = (sate = {}, action) => {
+const lessonsReducer = (state = lessons, action) => {
+    const { code } = action;
+    switch (action.type) {
+        case 'GO_TO':
+            return state.map(lesson => ({ code: lesson.code, title: lesson.title, display: lesson.code === code }));
+        default:
+            return state.map(lesson => ({ code: lesson.code, title: lesson.title, display: false }));
+    }
+};
+
+const lessonsApp = (state = {}, action) => {
     return {
-        lessons: [],
-        visibilityFilter: visibilityFilter(
-            state.visibilityFilter,
+        lessons: lessonsReducer(
+            state.lessons,
             action
         )
     };
 };
+
+const store = createStore(lessonsApp);
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('-------------');
+
+console.log('Dispatching GO_TO.');
+store.dispatch({ type: 'GO_TO', code: 'family' });
+console.log('Current state:');
+console.log(store.getState());
+console.log('-------------');
+
+console.log('Dispatching GO_TO.');
+store.dispatch({ type: 'GO_TO', code: 'greetings' });
+console.log('Current state:');
+console.log(store.getState());
+console.log('-------------');
